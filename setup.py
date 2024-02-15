b75ebc423d3e1eb215a867312d765cc334113835
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2024-02-15 21:01:00 +0100

    Update scripts/ for model training

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+from pathlib import Path
+
+this_directory = Path(__file__).resolve().parent
+long_description = (this_directory / "./README.md").read_text()
+setuptools.setup(
+	name="spliceai-framework",
+	version="0.0.2",
+	author="Kuan-Hao Chao, Alan Mao",
+	author_email="kh.chao@cs.jhu.edu",
+	description="A splice site preditction framework",
+	url="https://github.com/Kuanhao-Chao/spliceAI-MANE",
+	# install_requires=['numpy', "biopython", "cigar>=0.1.3", "parasail>=1.2.1", 'intervaltree>=3.1.0', 'interlap>=0.2.7', 'pyfaidx', 'pysam', 'gffutils', 'ujson'],
+	python_requires='>=3.6',
+	packages=setuptools.find_packages(),
+	entry_points={'console_scripts': ['spliceai-framework = spliceai-framework.lifton:main'], },
+        long_description=long_description,
+        long_description_content_type='text/markdown'
+)
