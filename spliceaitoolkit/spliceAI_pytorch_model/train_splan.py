5c8178f3d26f84bca2290399596743039263732a
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2024-02-25 11:58:15 -0500

    Add training script to test training on full SpliceAI27 dataset

--- spliceaitoolkit/spliceAI_pytorch_model/train_splan.py
+++ spliceaitoolkit/spliceAI_pytorch_model/train_splan.py
@@ -27,17 +27,15 @@ def setup_device():
     return torch.device(device_str)
 
 
-def initialize_paths(chunk_size, flanking_size, exp_num, target):
+def initialize_paths(project_root, project_name, chunk_size, flanking_size, exp_num, training_target, sequence_length):
     """Initialize project directories and create them if they don't exist."""
     ####################################
     # Modify the model verson here!!
     ####################################
-    MODEL_VERSION = f"{target}_splan_{chunk_size}chunk_{flanking_size}flank_spliceai_architecture"
+    MODEL_VERSION = f"{project_name}_{training_target}_{sequence_length}_{flanking_size}_{exp_num}"
     ####################################
     # Modify the model verson here!!
     ####################################
-    project_root = "/Users/chaokuan-hao/Documents/Projects/spliceAI-toolkit/"
-    data_dir = f"{project_root}results/{target}/"
     model_train_outdir = f"{project_root}results/model_train_outdir/{MODEL_VERSION}/{exp_num}/"
     model_output_base = f"{model_train_outdir}models/"
     log_output_base = f"{model_train_outdir}LOG/"
@@ -46,7 +44,7 @@ def initialize_paths(chunk_size, flanking_size, exp_num, target):
     log_output_test_base = f"{log_output_base}TEST/"
     for path in [model_output_base, log_output_train_base, log_output_val_base, log_output_test_base]:
         os.makedirs(path, exist_ok=True)
-    return data_dir, model_output_base, log_output_train_base, log_output_val_base, log_output_test_base
+    return model_output_base, log_output_train_base, log_output_val_base, log_output_test_base
 
 
 def initialize_model_and_optim(device, flanking_size):
@@ -268,106 +266,70 @@ def main():
     # os.environ['WANDB_MODE'] = 'disabled'
     parser = argparse.ArgumentParser()
     parser.add_argument('--disable-wandb', '-d', action='store_true', default=False)
+    parser.add_argument('--project-root', '-p', type=str)
+    parser.add_argument('--project-name', '-s', type=str)
     parser.add_argument('--flanking-size', '-f', type=int, default=80)
     parser.add_argument('--exp-num', '-e', type=str, default=0)
     parser.add_argument('--training-target', '-t', type=str, default="SpliceAI")
+    parser.add_argument('--train-dataset', '-train', type=str)
+    parser.add_argument('--test-dataset', '-test', type=str)
+    parser.add_argument('--dataset-name', '-dname', type=str)
     args = parser.parse_args()
-
-    print("args: ", args)
-
+    print("args: ", args, file=sys.stderr)
+    project_root = args.project_root
+    project_name = args.project_name
+    sequence_length = 5000
     flanking_size = int(args.flanking_size)
     exp_num = args.exp_num
     training_target = args.training_target
     assert int(flanking_size) in [80, 400, 2000, 10000]
-    assert training_target in ["MANE", "SpliceAI"]
+    assert training_target in ["MANE", "SpliceAI", "SpliceAI27"]
     if args.disable_wandb:
         os.environ['WANDB_MODE'] = 'disabled'
-    wandb.init(project=f'{training_target}_5000_{flanking_size}_{exp_num}', reinit=True)
+    wandb.init(project=f'{project_name}_{training_target}_{sequence_length}_{flanking_size}_{exp_num}', reinit=True)
     device = setup_device()
+    print("device: ", device, file=sys.stderr)
     # target = "train_test_dataset_SpliceAI27"
-    target = "train_test_dataset_MANE"
-    # target = f"train_test_dataset_{training_target}"
-    data_dir, model_output_base, log_output_train_base, log_output_val_base, log_output_test_base = initialize_paths(500, flanking_size, exp_num, target)
-    print("* data_dir: ", data_dir)
-    print("* model_output_base: ", model_output_base)
-    print("* log_output_train_base: ", log_output_train_base)
-    print("* log_output_val_base: ", log_output_val_base)
-    print("* log_output_test_base: ", log_output_test_base)
-    # training_dataset = f"{data_dir}dataset_train_all.h5"
-    # testing_dataset = f"{data_dir}dataset_test_0.h5"
-    training_dataset = f"{data_dir}dataset_train_500.h5"
-    testing_dataset = f"{data_dir}dataset_test_500.h5"
+    # target = "train_test_dataset_MANE"
+    dataset_name = args.dataset_name
+    model_output_base, log_output_train_base, log_output_val_base, log_output_test_base = initialize_paths(project_root, project_name, sequence_length, flanking_size, exp_num, training_target, sequence_length)
+    print("* model_output_base: ", model_output_base, file=sys.stderr)
+    print("* log_output_train_base: ", log_output_train_base, file=sys.stderr)
+    print("* log_output_val_base: ", log_output_val_base, file=sys.stderr)
+    print("* log_output_test_base: ", log_output_test_base, file=sys.stderr)
+    training_dataset = args.train_dataset
+    testing_dataset = args.test_dataset
+    print("training_dataset: ", training_dataset, file=sys.stderr)
+    print("testing_dataset: ", testing_dataset, file=sys.stderr)
     
     train_h5f = h5py.File(training_dataset, 'r')
     test_h5f = h5py.File(testing_dataset, 'r')
     batch_num = len(train_h5f.keys()) // 2
-    print("batch_num: ", batch_num)
+    print("batch_num: ", batch_num, file=sys.stderr)
     np.random.seed(RANDOM_SEED)  # You can choose any number as a seed
     idxs = np.random.permutation(batch_num)
     train_idxs = idxs[:int(0.9 * batch_num)]
     val_idxs = idxs[int(0.9 * batch_num):]
-
-    # train_h5f = h5py.File(training_dataset, 'r')
-    # test_h5f = h5py.File(testing_dataset, 'r')
-    # batch_num = len(test_h5f.keys()) // 2
-    # print("batch_num: ", batch_num)
-    # idxs = np.random.permutation(batch_num)
-    # train_idxs = idxs[:int(0.9 * batch_num)]
-    # val_idxs = idxs[int(0.9 * batch_num):]
-
-    # test_idxs = np.arange(2)
-    # train_idxs = idxs[:int(0.9 * batch_num)]
-    # val_idxs = idxs[int(0.9 * batch_num):]
     test_idxs = np.arange(len(test_h5f.keys()) // 2)
     model, criterion, optimizer, scheduler, params = initialize_model_and_optim(device, flanking_size)
     train_metric_files = {
-        # 'neither_precision': f'{log_output_train_base}/neither_precision.txt',
-        # 'neither_recall': f'{log_output_train_base}/neither_recall.txt',
-        # 'neither_f1': f'{log_output_train_base}/neither_f1.txt',
-        # 'neither_accuracy': f'{log_output_train_base}/neither_accuracy.txt',
         'topk_donor': f'{log_output_train_base}/donor_topk.txt',
-        # 'donor_precision': f'{log_output_train_base}/donor_precision.txt',
-        # 'donor_recall': f'{log_output_train_base}/donor_recall.txt',
-        # 'donor_f1': f'{log_output_train_base}/donor_f1.txt',
         'auprc_donor': f'{log_output_train_base}/donor_accuracy.txt',
         'topk_acceptor': f'{log_output_train_base}/acceptor_topk.txt',
-        # 'acceptor_precision': f'{log_output_train_base}/acceptor_precision.txt',
-        # 'acceptor_recall': f'{log_output_train_base}/acceptor_recall.txt',
-        # 'acceptor_f1': f'{log_output_train_base}/acceptor_f1.txt',
         'auprc_acceptor': f'{log_output_train_base}/acceptor_accuracy.txt',
         'loss': f'{log_output_train_base}/loss.txt'
     }
     valid_metric_files = {
-        # 'neither_precision': f'{log_output_val_base}/neither_precision.txt',
-        # 'neither_recall': f'{log_output_val_base}/neither_recall.txt',
-        # 'neither_f1': f'{log_output_val_base}/neither_f1.txt',
-        # 'neither_accuracy': f'{log_output_val_base}/neither_accuracy.txt',
         'topk_donor': f'{log_output_val_base}/donor_topk.txt',
-        # 'donor_precision': f'{log_output_val_base}/donor_precision.txt',
-        # 'donor_recall': f'{log_output_val_base}/donor_recall.txt',
-        # 'donor_f1': f'{log_output_val_base}/donor_f1.txt',
         'auprc_donor': f'{log_output_val_base}/donor_accuracy.txt',
         'topk_acceptor': f'{log_output_val_base}/acceptor_topk.txt',
-        # 'acceptor_precision': f'{log_output_val_base}/acceptor_precision.txt',
-        # 'acceptor_recall': f'{log_output_val_base}/acceptor_recall.txt',
-        # 'acceptor_f1': f'{log_output_val_base}/acceptor_f1.txt',
         'auprc_acceptor': f'{log_output_val_base}/acceptor_accuracy.txt',
         'loss': f'{log_output_val_base}/loss.txt'
     }
     test_metric_files = {
-        # 'neither_precision': f'{log_output_test_base}/neither_precision.txt',
-        # 'neither_recall': f'{log_output_test_base}/neither_recall.txt',
-        # 'neither_f1': f'{log_output_test_base}/neither_f1.txt',
-        # 'neither_accuracy': f'{log_output_test_base}/neither_accuracy.txt',
         'topk_donor': f'{log_output_test_base}/donor_topk.txt',
-        # 'donor_precision': f'{log_output_test_base}/donor_precision.txt',
-        # 'donor_recall': f'{log_output_test_base}/donor_recall.txt',
-        # 'donor_f1': f'{log_output_test_base}/donor_f1.txt',
         'auprc_donor': f'{log_output_test_base}/donor_accuracy.txt',
         'topk_acceptor': f'{log_output_test_base}/acceptor_topk.txt',
-        # 'acceptor_precision': f'{log_output_test_base}/acceptor_precision.txt',
-        # 'acceptor_recall': f'{log_output_test_base}/acceptor_recall.txt',
-        # 'acceptor_f1': f'{log_output_test_base}/acceptor_f1.txt',
         'auprc_acceptor': f'{log_output_test_base}/acceptor_accuracy.txt',
         'loss': f'{log_output_test_base}/loss.txt'
     }
@@ -375,9 +337,9 @@ def main():
         print("\n--------------------------------------------------------------")
         print(f">> Epoch {epoch + 1}")
         start_time = time.time()
-        train_epoch(model, train_h5f, train_idxs[:15], params["BATCH_SIZE"], criterion, optimizer, scheduler, device, params, train_metric_files, run_mode="train")
-        valid_epoch(model, train_h5f, val_idxs[:10], params["BATCH_SIZE"], criterion, device, params, valid_metric_files, run_mode="validation")
-        valid_epoch(model, test_h5f, test_idxs[:10], params["BATCH_SIZE"], criterion, device, params, test_metric_files, run_mode="test")
+        train_epoch(model, train_h5f, train_idxs, params["BATCH_SIZE"], criterion, optimizer, scheduler, device, params, train_metric_files, run_mode="train")
+        valid_epoch(model, train_h5f, val_idxs, params["BATCH_SIZE"], criterion, device, params, valid_metric_files, run_mode="validation")
+        valid_epoch(model, test_h5f, test_idxs, params["BATCH_SIZE"], criterion, device, params, test_metric_files, run_mode="test")
 
         # run_epoch(model, test_h5f, test_idxs, params["BATCH_SIZE"], criterion, None, None, device, params, test_metric_files, run_mode="test")
         torch.save(model.state_dict(), f"{model_output_base}/model_{epoch}.pt")
