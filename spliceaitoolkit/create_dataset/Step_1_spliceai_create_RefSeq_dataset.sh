0c9ac720bf1d465f56cc5ed9db6d18d026c7d38a
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2024-02-26 16:13:49 -0500

    Update steps to process RefSeq dataset

---
+++ spliceaitoolkit/create_dataset/Step_1_spliceai_create_RefSeq_dataset.sh
@@ -0,0 +1,5 @@
+python Step_1_create_datafile.py \
+--genome-fasta  /Users/chaokuan-hao/Documents/Projects/ref_genome/homo_sapiens/NCBI_Refseq_chr_fixed/GCF_000001405.40_GRCh38.p14_genomic.fna \
+--annotation-gff /Users/chaokuan-hao/Documents/Projects/ref_genome/homo_sapiens/NCBI_Refseq_chr_fixed/GCF_000001405.40-RS_2023_10/GCF_000001405.40_GRCh38.p14_genomic.gff \
+--output-dir /Users/chaokuan-hao/Documents/Projects/spliceAI-toolkit/results/train_test_dataset_RefSeq/ \
+> dataset_creation.log 2> dataset_creation_error.log
