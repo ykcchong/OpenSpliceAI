142f62bbeaf90ab326f276d57d7c41386e433366
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2024-02-26 09:20:37 -0500

    Update SpliceAI original training code & Pytorch training code

--- experiments/SpliceAI_train_code/Canonical/train_model.sh
+++ experiments/SpliceAI_train_code/Canonical/train_model.sh
@@ -1,8 +1,9 @@
 python train_model.py \
---output /Users/chaokuan-hao/Documents/Projects/splan/experiments/SpliceAI_train_code/model_train_outdir/ \
---project-name subset_training_dataset \
+--output /Users/chaokuan-hao/Documents/Projects/spliceAI-toolkit/experiments/SpliceAI_train_code/model_train_outdir/ \
+--project-name subset_training_dataset_multihead \
 --flanking-size 80 \
 --exp-num 0 \
---training-target SpliceAI27 \
 --train-dataset /Users/chaokuan-hao/Documents/Projects/spliceAI-toolkit/results/train_test_dataset_SpliceAI27/dataset_train_all.h5 \
---test-dataset /Users/chaokuan-hao/Documents/Projects/spliceAI-toolkit/results/train_test_dataset_SpliceAI27/dataset_test_0.h5 > model_train.log 2> model_train_error.log
+--test-dataset /Users/chaokuan-hao/Documents/Projects/spliceAI-toolkit/results/train_test_dataset_SpliceAI27/dataset_test_0.h5 \
+--model-type SpliceAI_Multihead \
+> model_train.log 2> model_train_error.log
--- spliceaitoolkit/spliceAI_pytorch_model/train_splan.sh
+++ spliceaitoolkit/spliceAI_pytorch_model/train_splan.sh
@@ -1,1 +1,24 @@
-python train_splan.py --flanking-size 80 --exp-num small_dataset_h5py_version --training-target SpliceAI27 --train-dataset /Users/chaokuan-hao/Documents/Projects/spliceAI-toolkit/results/train_test_dataset_SpliceAI27/dataset_train_all.h5 --test-dataset /Users/chaokuan-hao/Documents/Projects/spliceAI-toolkit/results/train_test_dataset_SpliceAI27/dataset_test_0.h5 --dataset-name train_test_dataset_SpliceAI27 --project-root /Users/chaokuan-hao/Documents/Projects/spliceAI-toolkit/ --project-name SpliceAI27_h5py_dataset > dataset_creation.log 2> dataset_creation_error.log
+##########################################
+# Train MANE dataset h5py_version
+##########################################
+python train_splan.py --flanking-size 80 \
+--exp-num full_dataset_h5py_version \
+--training-target MANE \
+--train-dataset /Users/chaokuan-hao/Documents/Projects/spliceAI-toolkit/results/train_test_dataset_MANE/dataset_train_all.h5 \
+--test-dataset /Users/chaokuan-hao/Documents/Projects/spliceAI-toolkit/results/train_test_dataset_MANE/dataset_test_0.h5 \
+--project-root /Users/chaokuan-hao/Documents/Projects/spliceAI-toolkit/ \
+--project-name MANE_h5py_dataset \
+> dataset_creation.log 2> dataset_creation_error.log
+
+# ##########################################
+# # Train SpliceAI27 dataset h5py_version
+# ##########################################
+# python train_splan.py --flanking-size 80 \
+# # --exp-num medium_dataset_h5py_version \
+# --exp-num full_dataset_h5py_version \
+# --training-target SpliceAI27 \
+# --train-dataset /Users/chaokuan-hao/Documents/Projects/spliceAI-toolkit/results/train_test_dataset_SpliceAI27/dataset_train_all.h5 \
+# --test-dataset /Users/chaokuan-hao/Documents/Projects/spliceAI-toolkit/results/train_test_dataset_SpliceAI27/dataset_test_0.h5 \
+# --project-root /Users/chaokuan-hao/Documents/Projects/spliceAI-toolkit/ \
+# --project-name SpliceAI27_h5py_dataset \
+# > dataset_creation.log 2> dataset_creation_error.log
