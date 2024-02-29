c71a9c31e641b40307732e97fc6684ff85d423ef
Alan Mao <52225670+am12@users.noreply.github.com>
2024-02-28 20:16:20 -0500

    constants

--- spliceaitoolkit/scripts_create_dataset_spliceAI/Step_1_grab_sequence.sh
+++ spliceaitoolkit/scripts_create_dataset_spliceAI/Step_1_grab_sequence.sh
@@ -7,7 +7,11 @@ CLl=$(($CLr+1))
 # First nucleotide not included by BEDtools
 
 cat $splice_table | awk -v CLl=$CLl -v CLr=$CLr '{print $3"\t"($5-CLl)"\t"($6+CLr)}' > temp.bed
+# cat $splice_table | awk -v CLl=$CLl -v CLr=$CLr '{print $1"\t"($4-CLl)"\t"($5+CLr)}' > temp.bed
 
-bedtools getfasta -bed temp.bed -fi $ref_genome -fo $sequence -tab
+# error checking
+awk '$2 >= 0 && $3 > $2' temp.bed > filtered.bed
 
-rm temp.bed
+bedtools getfasta -bed filtered.bed -fi $ref_genome -fo $sequence -tab
+
+rm temp.bed filtered.bed
