0b8c24ca69ba4aa5651cc4ffde62cc0400d2178c
Alan Mao <52225670+am12@users.noreply.github.com>
2024-03-06 08:38:51 -0500

    dataset creation streamlined

--- spliceaitoolkit/create_dataset_spliceAI/Step_1_grab_sequence.sh
+++ spliceaitoolkit/create_dataset_spliceAI/Step_1_grab_sequence.sh
@@ -2,16 +2,19 @@
 
 source constants.py
 
+# computing the left and right window size
+# NOTE: first nucleotide not included by BEDtools
 CLr=$((CL_max/2))
 CLl=$(($CLr+1))
-# First nucleotide not included by BEDtools
 
+# stores relevant information from the splice table into temp bed file
 cat $splice_table | awk -v CLl=$CLl -v CLr=$CLr '{print $3"\t"($5-CLl)"\t"($6+CLr)}' > temp.bed
-# cat $splice_table | awk -v CLl=$CLl -v CLr=$CLr '{print $1"\t"($4-CLl)"\t"($5+CLr)}' > temp.bed
 
 # error checking
 awk '$2 >= 0 && $3 > $2' temp.bed > filtered.bed
 
+# convert into a sequence file 
 bedtools getfasta -bed filtered.bed -fi $ref_genome -fo $sequence -tab
 
+# clean temp files
 rm temp.bed filtered.bed
