381f94f6cc3ea1db55ce63e8a422e148a9cdbf40
Alan Mao <52225670+am12@users.noreply.github.com>
2024-03-12 12:54:01 -0400

    copied from main

---
+++ spliceaitoolkit/predict/predict.py
@@ -0,0 +1,100 @@
+def predict():
+    print("Running SpliceAI-toolkit with 'predict' mode")
+
+
+def calculate_metrics(y_true, y_pred):
+    """Calculate metrics including precision, recall, f1-score, and accuracy."""
+    precision, recall, f1, _ = precision_recall_fscore_support(y_true, y_pred, average='binary', zero_division=0)
+    accuracy = accuracy_score(y_true, y_pred)
+    return precision, recall, f1, accuracy
+
+
+def threshold_predictions(y_probs, threshold=0.5):
+    """Threshold probabilities to get binary predictions."""
+    return (y_probs > threshold).astype(int)
+
+
+def load_data_from_shard(h5f, shard_idx, device, batch_size, params, shuffle=False):
+    X = h5f[f'X{shard_idx}'][:].transpose(0, 2, 1)
+    Y = h5f[f'Y{shard_idx}'][0, ...].transpose(0, 2, 1)
+    # print("\n\tX.shape: ", X.shape)
+    # print("\tY.shape: ", Y.shape)
+    X = torch.tensor(X, dtype=torch.float32)
+    Y = torch.tensor(Y, dtype=torch.float32)
+    ds = TensorDataset(X, Y)
+    # print("\rds: ", ds)
+    return DataLoader(ds, batch_size=batch_size, shuffle=shuffle, drop_last=True, pin_memory=True)
+    # return DataLoader(ds, batch_size=batch_size, shuffle=shuffle, drop_last=False, num_workers=8, pin_memory=True)
+
+
+def valid_epoch(model, h5f, idxs, batch_size, criterion, device, params, metric_files, run_mode):
+    print(f"\033[1m{run_mode.capitalize()}ing model...\033[0m")
+    model.eval()
+    running_loss = 0.0
+    np.random.seed(RANDOM_SEED)  # You can choose any number as a seed
+    shuffled_idxs = np.random.choice(idxs, size=len(idxs), replace=False)    
+    print("shuffled_idxs: ", shuffled_idxs)
+    batch_ylabel = []
+    batch_ypred = []
+    print_dict = {}
+    for i, shard_idx in enumerate(shuffled_idxs, 1):
+        print(f"Shard {i}/{len(shuffled_idxs)}")
+        loader = load_data_from_shard(h5f, shard_idx, device, batch_size, params, shuffle=False)
+        pbar = tqdm(loader, leave=False, total=len(loader), desc=f'Shard {i}/{len(shuffled_idxs)}')
+        for batch in pbar:
+            DNAs, labels = batch[0].to(device), batch[1].to(device)
+            # print("\n\tDNAs.shape: ", DNAs.shape)
+            # print("\tlabels.shape: ", labels.shape)
+            DNAs, labels = clip_datapoints(DNAs, labels, params["CL"], 2)
+            DNAs, labels = DNAs.to(torch.float32).to(device), labels.to(torch.float32).to(device)
+            # print("\n\tAfter clipping DNAs.shape: ", DNAs.shape)
+            # print("\tAfter clipping labels.shape: ", labels.shape)
+            yp = model(DNAs)
+            loss = categorical_crossentropy_2d(labels, yp)
+            # loss = criterion(labels, yp)  # Permuting to [batch_size, sequence_length, num_classes]
+            running_loss += loss.item()
+            # print("loss: ", loss.item())
+            batch_ylabel.append(labels.detach().cpu())
+            batch_ypred.append(yp.detach().cpu())
+            print_dict["loss"] = loss.item()
+            pbar.set_postfix(print_dict)
+            pbar.update(1)           
+        pbar.close()
+    batch_ylabel = torch.cat(batch_ylabel, dim=0)
+    batch_ypred = torch.cat(batch_ypred, dim=0)
+    is_expr = (batch_ylabel.sum(axis=(1,2)) >= 1).cpu().numpy()
+    if np.any(is_expr):
+        ############################
+        # Topk SpliceAI assessment approach
+        ############################
+        subset_size = 1000
+        indices = np.arange(batch_ylabel[is_expr].shape[0])
+        subset_indices = np.random.choice(indices, size=min(subset_size, len(indices)), replace=False)
+        Y_true_1 = batch_ylabel[is_expr][subset_indices, 1, :].flatten().cpu().detach().numpy()
+        Y_true_2 = batch_ylabel[is_expr][subset_indices, 2, :].flatten().cpu().detach().numpy()
+        Y_pred_1 = batch_ypred[is_expr][subset_indices, 1, :].flatten().cpu().detach().numpy()
+        Y_pred_2 = batch_ypred[is_expr][subset_indices, 2, :].flatten().cpu().detach().numpy()
+        acceptor_topkl_accuracy, acceptor_auprc = print_topl_statistics(np.asarray(Y_true_1),
+                            np.asarray(Y_pred_1), metric_files["topk_acceptor"], type='acceptor', print_top_k=True)
+        donor_topkl_accuracy, donor_auprc = print_topl_statistics(np.asarray(Y_true_2),
+                            np.asarray(Y_pred_2), metric_files["topk_donor"], type='donor', print_top_k=True)
+        for k, v in metric_files.items():
+            with open(v, 'a') as f:
+                if k == "loss":
+                    f.write(f"{running_loss}\n")
+                elif k == "topk_acceptor":
+                    f.write(f"{acceptor_topkl_accuracy}\n")
+                elif k == "topk_donor":
+                    f.write(f"{donor_topkl_accuracy}\n")
+                elif k == "auprc_acceptor":
+                    f.write(f"{acceptor_auprc}\n")
+                elif k == "auprc_donor":
+                    f.write(f"{donor_auprc}\n")
+        wandb.log({
+            f'{run_mode}/loss': loss.item(),
+            f'{run_mode}/topk_acceptor': acceptor_topkl_accuracy,
+            f'{run_mode}/topk_donor': donor_topkl_accuracy,
+            f'{run_mode}/auprc_acceptor': acceptor_auprc,
+            f'{run_mode}/auprc_donor': donor_auprc,
+        })
+        print("***************************************\n\n")
