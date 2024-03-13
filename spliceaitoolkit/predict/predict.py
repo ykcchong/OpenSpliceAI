3baa6ca5202ea5c0be0e3f7a5a8556883695d870
Alan Mao <52225670+am12@users.noreply.github.com>
2024-03-13 10:12:12 -0400

    predict args

--- spliceaitoolkit/predict/predict.py
+++ spliceaitoolkit/predict/predict.py
@@ -160,57 +160,6 @@ def valid_epoch(model, h5f, idxs, batch_size, criterion, device, params, metric_
     model_evaluation(batch_ylabel, batch_ypred, metric_files, run_mode, criterion)
 
 
-def train_epoch(model, h5f, idxs, batch_size, criterion, optimizer, scheduler, device, params, metric_files, run_mode, sample_freq):
-    print(f"\033[1m{run_mode.capitalize()}ing model...\033[0m")
-    model.train()
-    running_loss = 0.0
-    np.random.seed(RANDOM_SEED)  # You can choose any number as a seed
-    shuffled_idxs = np.random.choice(idxs, size=len(idxs), replace=False)
-    print("shuffled_idxs: ", shuffled_idxs)
-    batch_ylabel = []
-    batch_ypred = []
-    print_dict = {}
-    batch_idx = 0
-    for i, shard_idx in enumerate(shuffled_idxs, 1):
-        print(f"Shard {i}/{len(shuffled_idxs)}")
-        loader = load_data_from_shard(h5f, shard_idx, device, batch_size, params, shuffle=True)
-        pbar = tqdm(loader, leave=False, total=len(loader), desc=f'Shard {i}/{len(shuffled_idxs)}')
-        for batch in pbar:
-            DNAs, labels = batch[0].to(device), batch[1].to(device)
-            # print("\n\tDNAs.shape: ", DNAs.shape)
-            # print("\tlabels.shape: ", labels.shape)
-            DNAs, labels = clip_datapoints(DNAs, labels, params["CL"], 2)
-            DNAs, labels = DNAs.to(torch.float32).to(device), labels.to(torch.float32).to(device)
-            # print("\n\tAfter clipping DNAs.shape: ", DNAs.shape)
-            # print("\tAfter clipping labels.shape: ", labels.shape)
-            optimizer.zero_grad()
-            yp = model(DNAs)
-            if criterion == "cross_entropy_loss":
-                loss = categorical_crossentropy_2d(labels, yp)
-            elif criterion == "focal_loss":
-                loss = focal_loss(labels, yp)
-            # Logging loss for every update.
-            with open(metric_files["loss_every_update"], 'a') as f:
-                f.write(f"{loss.item()}\n")
-            wandb.log({
-                f'{run_mode}/loss_every_update': loss.item(),
-            })
-            # print("loss: ", loss.item())
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
-            batch_ylabel.append(labels.detach().cpu())
-            batch_ypred.append(yp.detach().cpu())
-            print_dict["loss"] = loss.item()
-            pbar.set_postfix(print_dict)
-            pbar.update(1)
-            batch_idx += 1
-            # if batch_idx % sample_freq == 0:
-            #     model_evaluation(batch_ylabel, batch_ypred, metric_files, run_mode)
-        pbar.close()
-    model_evaluation(batch_ylabel, batch_ypred, metric_files, run_mode, criterion)
-
-
 def predict(args):
     print("Running SpliceAI-toolkit with 'predict' mode")
 
@@ -234,33 +183,30 @@ def predict(args):
     print("* log_output_base: ", log_output_base, file=sys.stderr)
 
     print("Model architecture: ", model_arch, file=sys.stderr)
-    print("Loss function: ", args.loss, file=sys.stderr)
     print("Flanking sequence size: ", args.flanking_size, file=sys.stderr)
 
-    train_h5f = h5py.File(training_dataset, 'r')
-    test_h5f = h5py.File(testing_dataset, 'r')
 
-    batch_num = len(train_h5f.keys()) // 2
-    print("Batch_num: ", batch_num, file=sys.stderr)
-    np.random.seed(RANDOM_SEED)  # You can choose any number as a seed
-    idxs = np.random.permutation(batch_num)
-    train_idxs = idxs[:int(0.9 * batch_num)]
-    val_idxs = idxs[int(0.9 * batch_num):]
-    test_idxs = np.arange(len(test_h5f.keys()) // 2)
-    print("train_idxs: ", train_idxs, file=sys.stderr)
-    print("val_idxs: ", val_idxs, file=sys.stderr)
-    print("test_idxs: ", test_idxs, file=sys.stderr)
+    # batch_num = len(train_h5f.keys()) // 2
+    # print("Batch_num: ", batch_num, file=sys.stderr)
+    # np.random.seed(RANDOM_SEED)  # You can choose any number as a seed
+    # idxs = np.random.permutation(batch_num)
+    # train_idxs = idxs[:int(0.9 * batch_num)]
+    # val_idxs = idxs[int(0.9 * batch_num):]
+    # test_idxs = np.arange(len(test_h5f.keys()) // 2)
+    # print("train_idxs: ", train_idxs, file=sys.stderr)
+    # print("val_idxs: ", val_idxs, file=sys.stderr)
+    # print("test_idxs: ", test_idxs, file=sys.stderr)
 
     model = load_model(device, flanking_size, model_arch)
 
-    train_metric_files = {
-        'topk_donor': f'{log_output_train_base}/donor_topk.txt',
-        'auprc_donor': f'{log_output_train_base}/donor_accuracy.txt',
-        'topk_acceptor': f'{log_output_train_base}/acceptor_topk.txt',
-        'auprc_acceptor': f'{log_output_train_base}/acceptor_accuracy.txt',
-        'loss_batch': f'{log_output_train_base}/loss_batch.txt',
-        'loss_every_update': f'{log_output_train_base}/loss_every_update.txt'
-    }
+    # train_metric_files = {
+    #     'topk_donor': f'{log_output_train_base}/donor_topk.txt',
+    #     'auprc_donor': f'{log_output_train_base}/donor_accuracy.txt',
+    #     'topk_acceptor': f'{log_output_train_base}/acceptor_topk.txt',
+    #     'auprc_acceptor': f'{log_output_train_base}/acceptor_accuracy.txt',
+    #     'loss_batch': f'{log_output_train_base}/loss_batch.txt',
+    #     'loss_every_update': f'{log_output_train_base}/loss_every_update.txt'
+    # }
     valid_metric_files = {
         'topk_donor': f'{log_output_val_base}/donor_topk.txt',
         'auprc_donor': f'{log_output_val_base}/donor_accuracy.txt',
@@ -269,24 +215,24 @@ def predict(args):
         'loss_batch': f'{log_output_val_base}/loss_batch.txt',
         'loss_every_update': f'{log_output_val_base}/loss_every_update.txt'
     }
-    test_metric_files = {
-        'topk_donor': f'{log_output_test_base}/donor_topk.txt',
-        'auprc_donor': f'{log_output_test_base}/donor_accuracy.txt',
-        'topk_acceptor': f'{log_output_test_base}/acceptor_topk.txt',
-        'auprc_acceptor': f'{log_output_test_base}/acceptor_accuracy.txt',
-        'loss_batch': f'{log_output_test_base}/loss_batch.txt',
-        'loss_every_update': f'{log_output_test_base}/loss_every_update.txt'
-    }
+    # test_metric_files = {
+    #     'topk_donor': f'{log_output_test_base}/donor_topk.txt',
+    #     'auprc_donor': f'{log_output_test_base}/donor_accuracy.txt',
+    #     'topk_acceptor': f'{log_output_test_base}/acceptor_topk.txt',
+    #     'auprc_acceptor': f'{log_output_test_base}/acceptor_accuracy.txt',
+    #     'loss_batch': f'{log_output_test_base}/loss_batch.txt',
+    #     'loss_every_update': f'{log_output_test_base}/loss_every_update.txt'
+    # }
     SAMPLE_FREQ = 1000
-    for epoch in range(EPOCH_NUM):
-        print("\n--------------------------------------------------------------")
-        print(f">> Epoch {epoch + 1}")
-        start_time = time.time()
-        train_epoch(model, train_h5f, train_idxs, params["BATCH_SIZE"], args.loss, optimizer, scheduler, device, params, train_metric_files, run_mode="train", sample_freq=SAMPLE_FREQ)
-        valid_epoch(model, train_h5f, val_idxs, params["BATCH_SIZE"], args.loss, device, params, valid_metric_files, run_mode="validation", sample_freq=SAMPLE_FREQ)
-        valid_epoch(model, test_h5f, test_idxs, params["BATCH_SIZE"], args.loss, device, params, test_metric_files, run_mode="test", sample_freq=SAMPLE_FREQ)
-        torch.save(model.state_dict(), f"{model_output_base}/model_{epoch}.pt")
-        print("--- %s seconds ---" % (time.time() - start_time))
-        print("--------------------------------------------------------------")
-    train_h5f.close()
-    test_h5f.close()
+    # for epoch in range(EPOCH_NUM):
+    #     print("\n--------------------------------------------------------------")
+    #     print(f">> Epoch {epoch + 1}")
+    #     start_time = time.time()
+    #     # train_epoch(model, train_h5f, train_idxs, params["BATCH_SIZE"], args.loss, optimizer, scheduler, device, params, train_metric_files, run_mode="train", sample_freq=SAMPLE_FREQ)
+    #     # valid_epoch(model, train_h5f, val_idxs, params["BATCH_SIZE"], args.loss, device, params, valid_metric_files, run_mode="validation", sample_freq=SAMPLE_FREQ)
+    #     # valid_epoch(model, test_h5f, test_idxs, params["BATCH_SIZE"], args.loss, device, params, test_metric_files, run_mode="test", sample_freq=SAMPLE_FREQ)
+    #     torch.save(model.state_dict(), f"{model_output_base}/model_{epoch}.pt")
+    #     print("--- %s seconds ---" % (time.time() - start_time))
+    #     print("--------------------------------------------------------------")
+
+    valid_epoch(model)
