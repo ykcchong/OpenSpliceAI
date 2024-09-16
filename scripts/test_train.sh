3c7039039343dd278f31781a0052debe0b5ad6de
Kuanhao-Chao <kuanhao.chao@gmail.com>
2024-09-15 21:08:02 -0400

    Fix errors in fine-tune model and add arguments

--- scripts/test_train.sh
+++ scripts/test_train.sh
@@ -6,12 +6,9 @@ OUTPUT_FILE="$OUTPUT_DIR/output.log"
 ERROR_FILE="$OUTPUT_DIR/error.log"
 
 openspliceai train --flanking-size 80 \
---disable-wandb \
---exp-num test_dataset \
 --train-dataset ../results/create-data/dataset_train.h5 \
 --test-dataset ../results/create-data/dataset_test.h5 \
 --output-dir $OUTPUT_DIR \
 --project-name human_MANE_chr21_chr22_test \
 --random-seed 11 \
---model SpliceAI \
 --loss focal_loss > $OUTPUT_FILE 2> $ERROR_FILE
