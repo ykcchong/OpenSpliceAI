084533b222e65826487bd1414c246ab84f7f9c85
am12 <smao10@jh.edu>
2024-10-02 07:18:33 +0000

    Fix for extracting sequences w motifs

--- experiments/mutagenesis/experiment_2/get_inputs.py
+++ experiments/mutagenesis/experiment_2/get_inputs.py
@@ -1,12 +1,13 @@
 import argparse
 from pyfaidx import Fasta
 import gffutils
+import os
 
 donor_motifs = {}
 acceptor_motifs = {}    
 
 def get_sequence(seqid, center_pos, half_len, fasta):
-    start = center_pos - half_len + 1
+    start = center_pos - half_len
     end = center_pos + half_len
     if start < 1:
         start = 1
@@ -15,13 +16,26 @@ def get_sequence(seqid, center_pos, half_len, fasta):
     seq = fasta[seqid][start:end]
     return seq
 
-def extract_sequences(fasta_file, gff_file, output_donor, output_acceptor, seq_length=400):
+def extract_sequences(fasta_file, gff_file, output_donor, output_acceptor, seq_length=400, db_file='experiments/mutagenesis/experiment_2/data/chr1.db'):
     # Load the FASTA file
     fasta = Fasta(fasta_file, sequence_always_upper=True)
     
     # Create a gffutils database from the GFF file
-    db = gffutils.create_db(gff_file, dbfn=':memory:', force=True, keep_order=True,
-                            merge_strategy='create_unique', sort_attribute_values=True, verbose=True)
+    if os.path.exists(db_file):
+        print(f"Connecting to existing GFF database: {db_file}")
+        db = gffutils.FeatureDB(db_file)
+    else:
+        print(f"Creating new GFF database: {db_file}")
+        db = gffutils.create_db(
+            gff_file,
+            dbfn=db_file,
+            force=True,
+            keep_order=True,
+            merge_strategy='create_unique',
+            sort_attribute_values=True,
+            verbose=True
+        )
+        
     donor_seqs = []
     acceptor_seqs = []
 
@@ -45,47 +59,47 @@ def extract_sequences(fasta_file, gff_file, output_donor, output_acceptor, seq_l
                 end = exon.end
                 strand = exon.strand
 
-                # Get donor sequence (exon end is the donor site)
+                # Get donor sequence (exon end is the donor site, GT motif)
                 donor_seq = get_sequence(seqid, end, half_len, fasta)
-                if len(donor_seq) < 2:  # Ensure there's at least enough sequence to extract the motif
+                if len(donor_seq) < 2:
                     continue
                 if strand == "-":
                     donor_seq = donor_seq.reverse.complement
                 donor_seq = str(donor_seq)
                 
                 if strand == "-":
-                    # Count motifs
-                    mid_acceptor_seq = donor_seq[half_len-1:half_len+1]
-                    acceptor_motifs[mid_acceptor_seq] = acceptor_motifs.get(mid_acceptor_seq, 0) + 1
-                    # Write all acceptor sequences
-                    acceptor_seqs.append(f">{seqid}_acceptor_{start}\n{donor_seq}")
+                    # Check for AG motif (acceptor)
+                    mid_acceptor_seq = donor_seq[-2:]  # Get the last two bases for acceptor
+                    if mid_acceptor_seq == "AG":
+                        acceptor_motifs[mid_acceptor_seq] = acceptor_motifs.get(mid_acceptor_seq, 0) + 1
+                        acceptor_seqs.append(f">{seqid}_acceptor_{start}\n{donor_seq}")
                 else:
-                    # Count motifs
-                    mid_donor_seq = donor_seq[half_len-1:half_len+1]
-                    donor_motifs[mid_donor_seq] = donor_motifs.get(mid_donor_seq, 0) + 1
-                    # Write all donor sequences
-                    donor_seqs.append(f">{seqid}_donor_{end}\n{donor_seq}") 
+                    # Check for GT motif (donor)
+                    mid_donor_seq = donor_seq[-2:]  # Get the last two bases for donor
+                    if mid_donor_seq == "GT":
+                        donor_motifs[mid_donor_seq] = donor_motifs.get(mid_donor_seq, 0) + 1
+                        donor_seqs.append(f">{seqid}_donor_{end}\n{donor_seq}") 
 
-                # Get acceptor sequence (exon start is the acceptor site)
+                # Get acceptor sequence (exon start is the acceptor site, AG motif)
                 acceptor_seq = get_sequence(seqid, start, half_len, fasta)
-                if len(acceptor_seq) < 2:  # Ensure there's at least enough sequence to extract the motif
+                if len(acceptor_seq) < 2:
                     continue
                 if strand == "-":
                     acceptor_seq = acceptor_seq.reverse.complement
                 acceptor_seq = str(acceptor_seq)
 
                 if strand == "-":
-                    # Count motifs
-                    mid_donor_seq = acceptor_seq[half_len-1:half_len+1]
-                    donor_motifs[mid_donor_seq] = donor_motifs.get(mid_donor_seq, 0) + 1
-                    # Write all donor sequences
-                    donor_seqs.append(f">{seqid}_donor_{end}\n{acceptor_seq}")     
-                else:  
-                    # Count motifs
-                    mid_acceptor_seq = acceptor_seq[half_len-1:half_len+1]
-                    acceptor_motifs[mid_acceptor_seq] = acceptor_motifs.get(mid_acceptor_seq, 0) + 1
-                    # Write all acceptor sequences
-                    acceptor_seqs.append(f">{seqid}_acceptor_{start}\n{acceptor_seq}")
+                    # Check for GT motif (donor)
+                    mid_donor_seq = acceptor_seq[:2]  # Get the first two bases for donor
+                    if mid_donor_seq == "GT":
+                        donor_motifs[mid_donor_seq] = donor_motifs.get(mid_donor_seq, 0) + 1
+                        donor_seqs.append(f">{seqid}_donor_{end}\n{acceptor_seq}")
+                else:
+                    # Check for AG motif (acceptor)
+                    mid_acceptor_seq = acceptor_seq[:2]  # Get the first two bases for acceptor
+                    if mid_acceptor_seq == "AG":
+                        acceptor_motifs[mid_acceptor_seq] = acceptor_motifs.get(mid_acceptor_seq, 0) + 1
+                        acceptor_seqs.append(f">{seqid}_acceptor_{start}\n{acceptor_seq}")
         
     print("Donor motifs:")
     print(sorted(donor_motifs.items(), key=lambda x: x[1], reverse=True))
@@ -111,6 +125,6 @@ if __name__ == '__main__':
     
     fasta = '/ccb/cybertron/smao10/openspliceai/data/toy/human/chr1.fa'
     gff = '/ccb/cybertron/smao10/openspliceai/data/toy/human/chr1.gff'
-    donor = '/ccb/cybertron/smao10/openspliceai/experiments/mutagenesis/experiment_2/data/donor.fa'
-    acceptor = '/ccb/cybertron/smao10/openspliceai/experiments/mutagenesis/experiment_2/data/acceptor.fa'
+    donor = '/ccb/cybertron/smao10/openspliceai/experiments/mutagenesis/experiment_2/data/donor_new.fa'
+    acceptor = '/ccb/cybertron/smao10/openspliceai/experiments/mutagenesis/experiment_2/data/acceptor_new.fa'
     extract_sequences(fasta, gff, donor, acceptor)
