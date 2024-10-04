3920c00010e8d2f3915500ceed3731bba2797773
Kuanhao-Chao <kuanhao.chao@gmail.com>
2024-10-04 15:32:00 +0100

    Remove old OpenSpliceAI models and update fine-tune and train submodule

--- openspliceai/fine_tune/fine_tune.py
+++ openspliceai/fine_tune/fine_tune.py
@@ -16,7 +16,8 @@ import time
 from sklearn.metrics import precision_recall_fscore_support, accuracy_score
 
 
-def initialize_model_and_optim(device, flanking_size, pretrained_model, unfreeze, unfreeze_all):
+def initialize_model_and_optim(device, flanking_size, epochs, scheduler,
+                               pretrained_model, unfreeze, unfreeze_all):
     L = 32
     N_GPUS = 2
     W = np.asarray([11, 11, 11, 11])
@@ -78,10 +79,13 @@ def initialize_model_and_optim(device, flanking_size, pretrained_model, unfreeze
                 param.requires_grad = True
     # Set up optimizer and scheduler
     optimizer = optim.AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=2, verbose=True)
-    print(model, file=sys.stderr)    
+    if scheduler == "MultiStepLR":
+        scheduler_obj = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[epochs-4, epochs-3, epochs-2, epochs-1], gamma=0.5)
+    elif scheduler == "CosineAnnealingWarmRestarts":
+        scheduler_obj = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+            optimizer, T_0=5, T_mult=1, eta_min=1e-5, last_epoch=-1)    
     params = {'L': L, 'W': W, 'AR': AR, 'CL': CL, 'SL': SL, 'BATCH_SIZE': BATCH_SIZE, 'N_GPUS': N_GPUS}
-    return model, optimizer, scheduler, params
+    return model, optimizer, scheduler_obj, params
 
 
 def fine_tune(args):
@@ -91,163 +95,13 @@ def fine_tune(args):
     model_output_base, log_output_train_base, log_output_val_base, log_output_test_base = initialize_paths(args)
     train_h5f, test_h5f, batch_num = load_datasets(args)
     train_idxs, val_idxs, test_idxs = generate_indices(batch_num, args.random_seed, test_h5f)
-    model, optimizer, scheduler, params = initialize_model_and_optim(device, args.flanking_size, args.pretrained_model, args.unfreeze, args.unfreeze_all)
+    model, optimizer, scheduler, params = initialize_model_and_optim(device, args.flanking_size, args.epochs, args.scheduler, args.pretrained_model, args.unfreeze, args.unfreeze_all)
     
     params["RANDOM_SEED"] = args.random_seed
     train_metric_files = create_metric_files(log_output_train_base)
     valid_metric_files = create_metric_files(log_output_val_base)
     test_metric_files = create_metric_files(log_output_test_base)
-    train_model(model, optimizer, scheduler, train_h5f, test_h5f, train_idxs, val_idxs, test_idxs, 
-                model_output_base, args, device, params, train_metric_files, valid_metric_files, test_metric_files)
+    train_model(model, optimizer, scheduler, train_h5f, test_h5f, train_idxs, 
+                val_idxs, test_idxs, model_output_base, args, device, params, train_metric_files, valid_metric_files, test_metric_files)
     train_h5f.close()
     test_h5f.close()
-
-
-
-    # output_dir = args.output_dir
-    # project_name = args.project_name
-    # sequence_length = SL
-    # flanking_size = int(args.flanking_size)
-    # exp_num = args.exp_num
-    # pretrained_model = args.pretrained_model
-    # unfreeze = args.unfreeze
-    # assert int(flanking_size) in [80, 400, 2000, 10000]
-
-    # device = setup_device()
-    # print("device: ", device, file=sys.stderr)
-    # model_output_base, log_output_train_base, log_output_val_base, log_output_test_base = initialize_paths(output_dir, project_name, flanking_size, exp_num, sequence_length, args.loss, args.random_seed)
-    # print("* Project name: ", args.project_name, file=sys.stderr)
-    # print("* Model_output_base: ", model_output_base, file=sys.stderr)
-    # print("* Log_output_train_base: ", log_output_train_base, file=sys.stderr)
-    # print("* Log_output_val_base: ", log_output_val_base, file=sys.stderr)
-    # print("* Log_output_test_base: ", log_output_test_base, file=sys.stderr)
-
-    # training_dataset = args.train_dataset
-    # testing_dataset = args.test_dataset
-
-    # print("* Training_dataset: ", training_dataset, file=sys.stderr)
-    # print("* Testing_dataset: ", testing_dataset, file=sys.stderr)
-    # print("* Model architecture: ", pretrained_model, file=sys.stderr)
-    # print("* Loss function: ", args.loss, file=sys.stderr)
-    # print("* Flanking sequence size: ", args.flanking_size, file=sys.stderr)
-    # print("* Exp number: ", args.exp_num, file=sys.stderr)
-
-    # train_h5f = h5py.File(training_dataset, 'r')
-    # test_h5f = h5py.File(testing_dataset, 'r')
-    # batch_num = len(train_h5f.keys()) // 2
-    # RANDOM_SEED = args.random_seed
-
-    # print("* Batch_num: ", batch_num, file=sys.stderr)
-    # print("* RANDOM_SEED: ", RANDOM_SEED, file=sys.stderr)
-    # print("***************************************\n\n", file=sys.stderr)
-    # np.random.seed(RANDOM_SEED)
-
-    # idxs = np.random.permutation(batch_num)
-    # train_idxs = idxs[:int(0.9 * batch_num)]
-    # val_idxs = idxs[int(0.9 * batch_num):]
-    # test_idxs = np.arange(len(test_h5f.keys()) // 2)
-
-    # # print("train_idxs: ", train_idxs, file=sys.stderr)
-    # # print("val_idxs: ", val_idxs, file=sys.stderr)
-    # # print("test_idxs: ", test_idxs, file=sys.stderr)
-
-    # model, criterion, optimizer, scheduler, params = initialize_model_and_optim(device, flanking_size, pretrained_model, unfreeze, args.unfreeze_all)
-    # params["RANDOM_SEED"] = RANDOM_SEED
-    # train_metric_files = {
-    #     'donor_topk_all': f'{log_output_train_base}/donor_topk_all.txt',
-    #     'donor_topk': f'{log_output_train_base}/donor_topk.txt',
-    #     'donor_auprc': f'{log_output_train_base}/donor_auprc.txt',
-    #     'donor_accuracy': f'{log_output_train_base}/donor_accuracy.txt',
-    #     'donor_precision': f'{log_output_train_base}/donor_precision.txt',
-    #     'donor_recall': f'{log_output_train_base}/donor_recall.txt',
-    #     'donor_f1': f'{log_output_train_base}/donor_f1.txt',
-    #     'acceptor_topk_all': f'{log_output_train_base}/acceptor_topk_all.txt',
-    #     'acceptor_topk': f'{log_output_train_base}/acceptor_topk.txt',
-    #     'acceptor_auprc': f'{log_output_train_base}/acceptor_auprc.txt',
-    #     'acceptor_accuracy': f'{log_output_train_base}/acceptor_accuracy.txt',
-    #     'acceptor_precision': f'{log_output_train_base}/acceptor_precision.txt',
-    #     'acceptor_recall': f'{log_output_train_base}/acceptor_recall.txt',
-    #     'acceptor_f1': f'{log_output_train_base}/acceptor_f1.txt',
-    #     'accuracy': f'{log_output_train_base}/accuracy.txt',
-    #     'loss_batch': f'{log_output_train_base}/loss_batch.txt',
-    #     'loss_every_update': f'{log_output_train_base}/loss_every_update.txt',
-    # }
-    # valid_metric_files = {
-    #     'donor_topk_all': f'{log_output_val_base}/donor_topk_all.txt',
-    #     'donor_topk': f'{log_output_val_base}/donor_topk.txt',
-    #     'donor_auprc': f'{log_output_val_base}/donor_auprc.txt',
-    #     'donor_accuracy': f'{log_output_val_base}/donor_accuracy.txt',
-    #     'donor_precision': f'{log_output_val_base}/donor_precision.txt',
-    #     'donor_recall': f'{log_output_val_base}/donor_recall.txt',
-    #     'donor_f1': f'{log_output_val_base}/donor_f1.txt',
-    #     'acceptor_topk_all': f'{log_output_val_base}/acceptor_topk_all.txt',
-    #     'acceptor_topk': f'{log_output_val_base}/acceptor_topk.txt',
-    #     'acceptor_auprc': f'{log_output_val_base}/acceptor_auprc.txt',
-    #     'acceptor_accuracy': f'{log_output_val_base}/acceptor_accuracy.txt',
-    #     'acceptor_precision': f'{log_output_val_base}/acceptor_precision.txt',
-    #     'acceptor_recall': f'{log_output_val_base}/acceptor_recall.txt',
-    #     'acceptor_f1': f'{log_output_val_base}/acceptor_f1.txt',
-    #     'accuracy': f'{log_output_val_base}/accuracy.txt',
-    #     'loss_batch': f'{log_output_val_base}/loss_batch.txt',
-    #     'loss_every_update': f'{log_output_val_base}/loss_every_update.txt',
-    # }
-    # test_metric_files = {
-    #     'donor_topk_all': f'{log_output_test_base}/donor_topk_all.txt',
-    #     'donor_topk': f'{log_output_test_base}/donor_topk.txt',
-    #     'donor_auprc': f'{log_output_test_base}/donor_auprc.txt',
-    #     'donor_accuracy': f'{log_output_test_base}/donor_accuracy.txt',
-    #     'donor_precision': f'{log_output_test_base}/donor_precision.txt',
-    #     'donor_recall': f'{log_output_test_base}/donor_recall.txt',
-    #     'donor_f1': f'{log_output_test_base}/donor_f1.txt',
-    #     'acceptor_topk_all': f'{log_output_test_base}/acceptor_topk_all.txt',
-    #     'acceptor_topk': f'{log_output_test_base}/acceptor_topk.txt',
-    #     'acceptor_auprc': f'{log_output_test_base}/acceptor_auprc.txt',
-    #     'acceptor_accuracy': f'{log_output_test_base}/acceptor_accuracy.txt',
-    #     'acceptor_precision': f'{log_output_test_base}/acceptor_precision.txt',
-    #     'acceptor_recall': f'{log_output_test_base}/acceptor_recall.txt',
-    #     'acceptor_f1': f'{log_output_test_base}/acceptor_f1.txt',      
-    #     'accuracy': f'{log_output_test_base}/accuracy.txt',
-    #     'loss_batch': f'{log_output_test_base}/loss_batch.txt',
-    #     'loss_every_update': f'{log_output_test_base}/loss_every_update.txt',
-    # }
-
-    #     # 'topk_donor': f'{log_output_train_base}/donor_topk.txt',
-    #     # 'auprc_donor': f'{log_output_train_base}/donor_accuracy.txt',
-    #     # 'topk_acceptor': f'{log_output_train_base}/acceptor_topk.txt',
-    #     # 'auprc_acceptor': f'{log_output_train_base}/acceptor_accuracy.txt',
-    #     # 'loss_batch': f'{log_output_train_base}/loss_batch.txt',
-    #     # 'loss_every_update': f'{log_output_train_base}/loss_every_update.txt'
-
-
-    # best_val_loss = float('inf')
-    # epochs_no_improve = 0
-    # for epoch in range(EPOCH_NUM):
-    #     print("\n============================================================")
-    #     current_lr = optimizer.param_groups[0]['lr']
-    #     print(f">> Epoch {epoch + 1}; Current Learning Rate: {current_lr}")
-    #     start_time = time.time()
-    #     train_loss = train_epoch(model, train_h5f, train_idxs, params["BATCH_SIZE"], args.loss, optimizer, scheduler, device, params, train_metric_files, flanking_size, run_mode="train")
-    #     val_loss = valid_epoch(model, train_h5f, val_idxs, params["BATCH_SIZE"], args.loss, device, params, valid_metric_files, flanking_size, run_mode="validation")
-    #     test_loss = valid_epoch(model, test_h5f, test_idxs, params["BATCH_SIZE"], args.loss, device, params, test_metric_files, flanking_size, run_mode="test")
-
-    #     print(f"Training Loss: {train_loss}")
-    #     print(f"Validation Loss: {val_loss}")
-    #     print(f"Testing Loss: {test_loss}")
-        
-    #     if args.early_stopping:
-    #         scheduler.step(val_loss)
-    #         if val_loss.item() < best_val_loss:
-    #             best_val_loss = val_loss.item()
-    #             torch.save(model.state_dict(), f"{model_output_base}/model_{epoch}.pt")
-    #             print("New best model saved.")
-    #             epochs_no_improve = 0
-    #         else:
-    #             epochs_no_improve += 1
-    #             print(f"No improvement in validation loss for {epochs_no_improve} epochs.")
-    #             if epochs_no_improve >= args.patience:
-    #                 print("Early stopping triggered.")
-    #                 break
-    #     print("--- %s seconds ---" % (time.time() - start_time))
-    #     print("============================================================")
-    # train_h5f.close()
-    # test_h5f.close()
