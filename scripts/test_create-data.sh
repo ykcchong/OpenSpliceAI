39dcce6f4f42259f65e9befa225c5e67145cc3ad
Kuanhao-Chao <kuanhao.chao@gmail.com>
2024-10-24 15:09:46 -0400

    Update experiments/ & scripts/

--- scripts/test_create-data.sh
+++ scripts/test_create-data.sh
@@ -9,6 +9,8 @@ ERROR_FILE="$OUTPUT_DIR/error.log"
 # Creating MANE dataset
 ########################################
 openspliceai create-data \
+--min-identity 0.8 \
+--min-coverage 0.01 \
 --genome-fasta  ../data/GRCh38_chr21_chr22.fna \
 --annotation-gff ../data/MANE.GRCh38.v1.3.refseq_genomic_chr21_chr22.gff \
 --output-dir $OUTPUT_DIR \
