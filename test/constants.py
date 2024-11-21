5c03ea0ecf877b1ffff63d0db9e5fa0c111efec9
Kuanhao-Chao <kuanhao.chao@gmail.com>
2024-11-21 15:20:56 -0500

    Add test example to load model and do prediction

---
+++ test/constants.py
@@ -0,0 +1,14 @@
+CL_max=10000
+# Maximum nucleotide context length (CL_max/2 on either side of the 
+# position of interest)
+# CL_max should be an even number
+
+SL=5000
+# Sequence length of SpliceAIs (SL+CL will be the input length and
+# SL will be the output length)
+#############################
+# Global variable definition
+############################## 
+EPOCH_NUM = 10
+
+
