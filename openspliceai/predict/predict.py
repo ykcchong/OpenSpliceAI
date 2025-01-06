9a193f681a01aefbb03a2cb270436ae1eefd6952
am12 <smao10@jh.edu>
2025-01-06 08:19:25 +0000

    Set to 0-based BED indexing

--- openspliceai/predict/predict.py
+++ openspliceai/predict/predict.py
@@ -804,17 +804,17 @@ def write_batch_to_bed(seq_name, gene_predictions, acceptor_bed, donor_bed, thre
                 acceptor_score = acceptor_scores[pos]
                 donor_score = donor_scores[pos]
                 if acceptor_score > threshold:
-                    acceptor_bed.write(f"{chrom}\t{start+pos-2}\t{start+pos}\t{name}_Acceptor\t{acceptor_score:.6f}\t{strand}\n")
+                    acceptor_bed.write(f"{chrom}\t{start+pos-3}\t{start+pos-1}\t{name}_Acceptor\t{acceptor_score:.6f}\t{strand}\n")
                 if donor_score > threshold:
-                    donor_bed.write(f"{chrom}\t{start+pos+1}\t{start+pos+3}\t{name}_Donor\t{donor_score:.6f}\t{strand}\n")
+                    donor_bed.write(f"{chrom}\t{start+pos}\t{start+pos+2}\t{name}_Donor\t{donor_score:.6f}\t{strand}\n")
         elif strand == '-':
             for pos in range(len(acceptor_scores)):
-                acceptor_score = donor_scores[pos]
-                donor_score = acceptor_scores[pos]
+                acceptor_score = acceptor_scores[pos]
+                donor_score = donor_scores[pos]
                 if acceptor_score > threshold:
-                    acceptor_bed.write(f"{chrom}\t{end-pos+1}\t{end-pos+3}\t{name}_Acceptor\t{acceptor_score:.6f}\t{strand}\n")
+                    acceptor_bed.write(f"{chrom}\t{end-pos}\t{end-pos+2}\t{name}_Acceptor\t{acceptor_score:.6f}\t{strand}\n")
                 if donor_score > threshold:
-                    donor_bed.write(f"{chrom}\t{end-pos-2}\t{end-pos}\t{name}_Donor\t{donor_score:.6f}\t{strand}\n")
+                    donor_bed.write(f"{chrom}\t{end-pos-3}\t{end-pos-1}\t{name}_Donor\t{donor_score:.6f}\t{strand}\n")
         else:
             print(f'\t[ERR] Undefined strand {strand}. Skipping {seq_name} batch...')
 
@@ -823,25 +823,13 @@ def write_batch_to_bed(seq_name, gene_predictions, acceptor_bed, donor_bed, thre
         strand = seq_name[-1] # use the ending as the strand (when lack other information)
         
         # write to file using absolute coordinates (using input FASTA as coordinates rather than GFF)
-        if strand == '+':
-            for pos in range(len(acceptor_scores)):
-                acceptor_score = acceptor_scores[pos]
-                donor_score = donor_scores[pos]
-                if acceptor_score > threshold:
-                    acceptor_bed.write(f"{chrom}\t{pos-2}\t{pos}\t{seq_name}_Acceptor\t{acceptor_score:.6f}\t{strand}\tabsolute_coordinates\n")
-                if donor_score > threshold:
-                    donor_bed.write(f"{chrom}\t{pos+1}\t{pos+3}\t{seq_name}_Donor\t{donor_score:.6f}\t{strand}\tabsolute_coordinates\n")
-        elif strand == '-':
-            for pos in range(len(acceptor_scores)):
-                acceptor_score = donor_scores[pos]
-                donor_score = acceptor_scores[pos]
-                if acceptor_score > threshold:
-                    acceptor_bed.write(f"{chrom}\t{pos-2}\t{pos}\t{seq_name}_Acceptor\t{acceptor_score:.6f}\t{strand}\tabsolute_coordinates\n")
-                if donor_score > threshold:
-                    donor_bed.write(f"{chrom}\t{pos+1}\t{pos+3}\t{seq_name}_Donor\t{donor_score:.6f}\t{strand}\tabsolute_coordinates\n")
-        else:
-            print(f'\t[ERR] Undefined strand {strand}. Skipping {seq_name} batch...')
-
+        for pos in range(len(acceptor_scores)):
+            acceptor_score = acceptor_scores[pos]
+            donor_score = donor_scores[pos]
+            if acceptor_score > threshold:
+                acceptor_bed.write(f"{chrom}\t{pos-3}\t{pos-1}\t{seq_name}_Acceptor\t{acceptor_score:.6f}\t{strand}\tabsolute_coordinates\n")
+            if donor_score > threshold:
+                donor_bed.write(f"{chrom}\t{pos}\t{pos-2}\t{seq_name}_Donor\t{donor_score:.6f}\t{strand}\tabsolute_coordinates\n")
 
 # NOTE: need to handle naming when gff file not provided.
 def generate_bed(predict_file, NAME, LEN, output_dir, threshold=1e-6, batch_ypred=None, debug=False):
