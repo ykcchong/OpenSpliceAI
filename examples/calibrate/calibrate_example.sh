ece690c45876ace15ff1bfe054b70d46746d6faa
Kuanhao-Chao <kuanhao.chao@gmail.com>
2025-01-27 19:27:00 +0100

    Update 'create-data', 'train', and 'calibrate' examples and remove toy data

--- examples/calibrate/calibrate_example.sh
+++ examples/calibrate/calibrate_example.sh
@@ -5,11 +5,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PDIR="$(dirname "$(dirname "$SCRIPT_DIR")")"
 
-echo "$PDIR"
-
 CURRENT_SPECIES="MANE"
 FLANKING_SIZE=10000
-RANDOM_SEED=11
 
 # Set up output directory
 OUTPUT_DIR="$PDIR/examples/calibrate/results/"
@@ -18,11 +15,14 @@ OUTPUT_FILE="$OUTPUT_DIR/output.log"
 ERROR_FILE="$OUTPUT_DIR/error.log"
 
 # Set up dataset paths
-TRAIN_DATASET="$PDIR/examples/create-data/results/dataset_train.h5"
-TEST_DATASET="$PDIR/examples/create-data/results/dataset_test.h5"
-CURRENT_SPECIES_lowercase="${CURRENT_SPECIES,,}"
+# TRAIN_DATASET="$PDIR/examples/create-data/results/dataset_train.h5"
+# TEST_DATASET="$PDIR/examples/create-data/results/dataset_test.h5"
 
-PRETRAINED_MODEL="$PDIR/examples/train/results/model.pt"
+TRAIN_DATASET="/home/kchao10/data_ssalzbe1/khchao/data/train_test_dataset_MANE/dataset_train.h5"
+TEST_DATASET="/home/kchao10/data_ssalzbe1/khchao/data/train_test_dataset_MANE/dataset_test.h5"
+PRETRAINED_MODEL="$PDIR/models/spliceai-mane/10000nt/model_10000nt_rs10.pt"
+
+CURRENT_SPECIES_lowercase="${CURRENT_SPECIES,,}"
 
 echo "Current species: $CURRENT_SPECIES"
 echo "Flanking size: $FLANKING_SIZE"
@@ -33,11 +33,10 @@ echo "Test dataset: $TEST_DATASET"
 echo "Pretrained model: $PRETRAINED_MODEL"
 
 # Run the OpenSpliceAI calibrate command
-echo openspliceai calibrate --flanking-size $FLANKING_SIZE \
+openspliceai calibrate --flanking-size $FLANKING_SIZE \
     --train-dataset $TRAIN_DATASET \
     --test-dataset $TEST_DATASET \
     --output-dir $OUTPUT_DIR \
     --project-name human_${CURRENT_SPECIES}_calibrate \
-    --random-seed ${RANDOM_SEED} \
     --pretrained-model $PRETRAINED_MODEL \
     --loss cross_entropy_loss > $OUTPUT_FILE 2> $ERROR_FILE
